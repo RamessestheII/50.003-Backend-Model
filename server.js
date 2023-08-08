a92b738f4fc8414a73aef36085fa00445fb1b9b2
AxeOilCeo <valerie_lim@mymail.sutd.edu.sg>
2023-08-08 10:16:57 +0800

    revert here in case error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 //Connect Database
 connectDB();
 
-//Init Middleware
+//Init Middleware (Bpdyparser)
 app.use(express.json({extended: false}));
 
 app.get("/", (req, res) => {
